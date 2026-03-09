6ac967ad6a8d234b8cac17b6ee6802c56c3cd4b6
wajii87 <wajidkhanali87@gmail.com>
2026-03-10 00:17:32 +0500

    Add files via upload

---
+++ Lab 1/Lab 1..cpp
@@ -0,0 +1,9 @@
+#include <iostream>      //  use for input and output
+
+using namespace std;     // use for writing cout instead of std::cout
+
+int main()               // Main function where program starts
+{
+    cout << "Hello World";   // Prints Hello World on the screen
+    return 0;                // Ends the program
+}
