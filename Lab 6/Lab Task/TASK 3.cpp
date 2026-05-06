a2cea516e7e4c6685a6319d4acb050950fcf04b5
wajii87 <wajidkhanali87@gmail.com>
2026-05-06 13:34:58 -0700

    Add files via upload

---
+++ Lab 6/Lab Task/TASK 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, sum = 0;
+
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+    for(int i = 1; i <= num; i++) {
+        sum += i;
+    }
+
+    cout << "Sum from 1 to " << num << " = " << sum << endl;
+
+    return 0;
+}
+
+
