8a276074ef96a46dc4ef2e133eb7e00e8f01bf8a
wajii87 <wajidkhanali87@gmail.com>
2026-05-18 15:11:27 -0700

    Add files via upload

---
+++ Lab 11/TASK/LAB TASK 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    // Declare and initialize a string variable
+    string message = "Welcome to C++ Programming";
+
+    // Display the string
+    cout << "Message: " << message << endl;
+
+    // Calculate and display the length of the string
+    cout << "Length of the string: " << message.length() << endl;
+
+    return 0;
+}
---
+++ Lab 11/TASK/LAB TASK 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Declare and initialize string variables
+    string firstName = "MUHAMMAD";
+    string lastName = "AWAIS";
+
+    // Concatenate the strings
+    string fullName = firstName + " " + lastName;
+
+    // Display the full name
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
